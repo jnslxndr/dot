e1f2f43cb9cbcb8aea6d837fc2a05dcdc9ebc336
jens-a-e <jens@lea.io>
2012-12-28 13:32:40 +0100

    Initial commit with the most needed stuff

---
+++ linkall.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# ================================
+# = THIS SCRIPT IS NOT FINISHED! =
+# ================================
+
+export DOT_DIR=${HOME}/dot
+
+function linkDotFile() {  
+  target=${HOME}/"."$1
+  source=${DOT_DIR}/files/$1
+  
+  echo "Linking "$source" to  "$target
+  ln -s $source $target
+}
+
+for df in `ls files`;
+do
+  linkDotFile $df
+done
